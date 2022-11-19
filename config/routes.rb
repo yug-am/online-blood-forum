d7e9360412b7064dd3ca97cfb0a66dba29f9f11b
Yugam <yugam11parashar@gmail.com>
2022-11-19 23:14:54 +0530

    :lipstick:bootstrap starter code, Admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'admin/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    get "/dashboard", to: "admin#dashboard"
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "admin#dashboard"
 end
