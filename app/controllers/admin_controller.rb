a0fda1da865ebb2d2eb73180f493f7f4f1833f91
Yugam <yugam11parashar@gmail.com>
2022-11-20 16:29:46 +0530

    Displaying last allocaion details:children_crossing:

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,19 @@
 class AdminController < ApplicationController
   def dashboard
-     
-     # puts Request.all
-    @requests = Request.all
-    #@donors = Donor.all
+    @requests=Request.all
+     @request_allo=""
+     @donor_allo=""
+     @donor_id_allo=""
+     @last_allocation=""
+    if params[:did]
+      @allocation_here=true
+       @request_allo =Request.find_by(requestID:params[:rid]  )
+       @donor_allo = Donor.find_by(donorID:params[:did])
+       @donor_id_allo= @donor_allo.donorID
+       @last_allocation=@donor_allo.last_allocation
 
+    end
 
-  #@requests
   end
   def handle_req
     @request =Request.find_by(requestID:params[:id])
@@ -14,6 +21,16 @@ class AdminController < ApplicationController
     #@donorsPost.where(user_id: @user.id)
     @donors=Donor.where(bloodGroup:@gp)
     #@request =Request.find(params[:id]) 
-end
 
-end
+    end
+  def allocate(rid,did)
+    #params.require(:rid, :did)
+    request =Request.find_by(requestID:params[:rid]  )
+    donor = Donor.where(donorID:params[:did])
+    puts request
+    puts donor
+    puts rid 
+    puts did
+    end
+
+  end
