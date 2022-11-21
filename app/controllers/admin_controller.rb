f95c49738ccf98e360845a6dc05c2059031fcf4b
Yugam <yugam11parashar@gmail.com>
2022-11-21 12:38:01 +0530

    :sparkles:Delete request function implemented

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,53 +1,55 @@
 class AdminController < ApplicationController
-  def error_pg
-  end
-  def dashboard
-    $is_admin=true
-    dig=4
-    init=10**(dig-1)
-    final=10**(dig)-1
-    @rand_conf_num=rand(init...final)
-     @requests=Request.all
-     @request_allo=""
-     @donor_allo=""
-     @donor_id_allo=""
-     @last_allocation=""
-    if $is_admin
-   
-    if params[:did]
-      @request_allo =Request.find_by(requestID:params[:rid]  )
-      if not @request_allo.isConfirmed
-      @allocation_here=true
-       
-       @donor_allo = Donor.find_by(id:params[:did])
-       @donor_id_allo= @donor_allo.id
+def error_pg
+end
+def dashboard
+$is_admin = true
+dig = 4
+init = 10 ** (dig - 1)
+final = 10 ** (dig) - 1
+@rand_conf_num = rand(init...final)
+@requests = Request.all
+@request_allo = ""
+@donor_allo = ""
+@donor_id_allo = ""
+@last_allocation = ""
+if $is_admin
 
-       #user = User.find_by(name: 'David')
-      @donor_allo.update(isUsed: true)
-        @request_allo.update(isConfirmed: true,allocationCode:@rand_conf_num)
+if params[:did]
+@request_allo = Request.find_by(requestID: params[:rid])
+if not @request_allo.isConfirmed
+@allocation_here = true
+if params[:did] =="-404"
+@donor_id_allo = -404
+@request_allo.update(isConfirmed: true, allocationCode: -404)
+else
+  @donor_allo = Donor.find_by(id: params[:did])
+@donor_id_allo = @donor_allo.id
+@donor_allo.update(isUsed: true)
+@request_allo.update(isConfirmed: true, allocationCode: @rand_conf_num)
+end
+temp_time = @request_allo.updated_at.in_time_zone(TZInfo::Timezone.get('Asia/Kolkata'))
+@last_allocation = temp_time.to_time.strftime("%-I:%M %p, %dth %B '%Y")
 
+#user = User.find_by(name: 'David')
 
-       temp_time=@donor_allo.updated_at.in_time_zone(TZInfo::Timezone.get('Asia/Kolkata'))
-       @last_allocation= temp_time.to_time.strftime("at %-I:%M %p, %dth %B '%Y")
-     end
-     end
-      end
+end
+end
+end
 
-  end
-  def handle_req
-    @request=""
-    @donors="" 
-    if $is_admin
-    @donors=Donor.all
-    
-    @request =Request.find_by(requestID:params[:id])
-    @gp=@request.bloodType
-    #@donorsPost.where(user_id: @user.id)
-    @donors=Donor.where(bloodGroup:@gp)
-    @donors=@donors.where(isUsed:false)
-    #@request =Request.find(params[:id]) 
-  end
-    end
-  
+end
+def handle_req
+@request = ""
+@donors = ""
+if $is_admin
+@donors = Donor.all
 
-  end
+@request = Request.find_by(requestID: params[:id])
+@gp = @request.bloodType
+# @donorsPost.where(user_id: @user.id)
+@donors = Donor.where(bloodGroup: @gp)
+@donors = @donors.where(isUsed: false)
+# @request = Request.find(params[: id])
+end
+end
+
+end
